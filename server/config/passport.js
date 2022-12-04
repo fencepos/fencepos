ba32fbe55b8f28f0f1c6cbd2e3f7a984d539faf4
nerdylive123 <70305325+nerdylive123@users.noreply.github.com>
2022-12-04 17:27:45 +0700

    add passport.js config, not yet configured

---
+++ server/config/passport.js
@@ -0,0 +1,23 @@
+const passport = require("passport");
+const {User} = require("../models/auth/User");
+const JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+let opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'secret';
+
+passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    User.findOne({id: jwt_payload.sub}, function(err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+}));
+
+module.exports = {passport}
