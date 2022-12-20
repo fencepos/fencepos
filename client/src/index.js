c917639e418b890854a65266a767d446651673e9
nerdylive123 <nerdylive123@users.noreply.github.com>
2022-12-20 19:09:23 +0800

    Added eslint and applied some style fixes

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import LoginPage from "./login/LoginPage";
 import reportWebVitals from './reportWebVitals';
 
 const router = createBrowserRouter([
-    { path: '/', element: <App /> },
-    { path: '/login', element: <LoginPage /> }
+  {path: '/', element: <App/>},
+  {path: '/login', element: <LoginPage/>}
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
--- client/src/reportWebVitals.js
+++ client/src/reportWebVitals.js
@@ -1,13 +1,18 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
-};
+let reportWebVitals;
+
+(async () => {
+  const { getCLS, getFID, getFCP, getLCP, getTTFB } = await import('web-vitals');
+  const webVitals = { getCLS, getFID, getFCP, getLCP, getTTFB };
+
+  reportWebVitals = (onPerfEntry) => {
+    if (onPerfEntry && onPerfEntry instanceof Function) {
+      webVitals.getCLS(onPerfEntry);
+      webVitals.getFID(onPerfEntry);
+      webVitals.getFCP(onPerfEntry);
+      webVitals.getLCP(onPerfEntry);
+      webVitals.getTTFB(onPerfEntry);
+    }
+  };
+})();
 
 export default reportWebVitals;
