93b3ba1c313b893d44282ee4fbce17b648f270f3
nerdylive123 <nerdylive123@users.noreply.github.com>
2022-12-20 21:09:38 +0800

    Fix reportWebVitals.js error

--- client/src/reportWebVitals.js
+++ client/src/reportWebVitals.js
@@ -1,7 +1,7 @@
 let reportWebVitals;
 
-(async () => {
-  const { getCLS, getFID, getFCP, getLCP, getTTFB } = await import('web-vitals');
+(() => {
+  const { getCLS, getFID, getFCP, getLCP, getTTFB } = import('web-vitals');
   const webVitals = { getCLS, getFID, getFCP, getLCP, getTTFB };
 
   reportWebVitals = (onPerfEntry) => {
